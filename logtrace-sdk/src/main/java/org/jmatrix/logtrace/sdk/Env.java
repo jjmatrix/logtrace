b9fab57f9c971778683b64d3abbb56f0ee79d19c
jjmatrix <jmatrix@vip.qq.com>
2016-02-08 23:27:54 +0800

    sdk

---
+++ logtrace-sdk/src/main/java/org/jmatrix/logtrace/sdk/Env.java
@@ -0,0 +1,26 @@
+package org.jmatrix.logtrace.sdk;
+
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author: jmatrix
+ * @date: 16/1/19
+ */
+public class Env {
+
+    private static final Logger logger = LoggerFactory.getLogger(Env.class);
+
+    public static boolean isDebug() {
+        String isDebug = System.getProperty("debug", "");
+        if (!StringUtils.isEmpty(isDebug)) {
+            try {
+                return Boolean.parseBoolean(isDebug);
+            } catch (Exception e) {
+                logger.error("parse debug parameter error.", e);
+            }
+        }
+        return false;
+    }
+}

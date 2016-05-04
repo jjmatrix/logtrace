67924b08a2ef92e2a9824fa2bf0b6ec4e21d5861
jjmatrix <jmatrix@vip.qq.com>
2016-05-04 13:04:36 +0800

    use junit4 feature to test exception

--- logtrace-client/src/test/java/org/jmatrix/logtrace/client/util/ZkUtilsTest.java
+++ logtrace-client/src/test/java/org/jmatrix/logtrace/client/util/ZkUtilsTest.java
@@ -1,14 +1,18 @@
 package org.jmatrix.logtrace.client.util;
 
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.Matcher;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 /**
  * @author: jmatrix
  * @date: 16/1/19
  */
-public class ZkUtilsTest{
+public class ZkUtilsTest {
 
     @Before
     public void setUp() throws Exception {
@@ -18,6 +22,18 @@ public class ZkUtilsTest{
     @Test(expected = IllegalArgumentException.class)
     public void testGetZkPathNullThrowException() throws Exception {
         String zkPath = null;
+        ZkUtils.getZkPath(zkPath);
+    }
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Test
+    public void testGetZkPathThrowExceptionMsg() throws Exception {
+        String zkPath = "";
+        thrown.expect(IllegalArgumentException.class);
+        thrown.expectMessage("empty");
+        thrown.expectMessage(CoreMatchers.containsString("zkPath"));
         ZkUtils.getZkPath(zkPath);
     }
 
